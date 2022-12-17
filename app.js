32979036de0d5e669c3557f977220503d197c0ca
dhrubobarman <dhrubobarman847@gmail.com>
2022-12-17 17:43:39 +0530

    docs

--- app.js
+++ app.js
@@ -16,6 +16,11 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+// swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
@@ -36,8 +41,9 @@ app.use(xss());
 
 // routes
 app.get("/", (req, res) => {
-  res.send("jobs api");
+  res.send("<h1>Jobs API</h1><a href='/api-docs'>Documentation</a>");
 });
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
